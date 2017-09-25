fff40f5f5377e5520d713b9b72e5ec4dc3d4874a
Chen-Yu Li <dodo5575@gmail.com>
2017-09-25 16:39:51 -0700

    add kafka_producer.py

---
+++ kafka/kafka_producer.py
@@ -0,0 +1,34 @@
+import random
+import sys
+import datetime
+import numpy as np
+from kafka import KafkaProducer
+from time import sleep
+
+
+userNum = sys.argv[1]
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+count = 0
+while True:
+    for userid_field in userNum:
+        time= datetime.datetime.now() 
+        time_field = time.strftime("%Y-%m-%d %H:%M:%S")
+    
+        acc_field = np.random.randn()
+        if count % 100 == 0:
+            acc_field += 10  # Add anomaly
+
+        message_info = '{"userid": "%s", "time": "%s", "acc": "%s"}' % (userid_field, time_field, acc_field)
+    
+        print(message_info)
+        producer.send('data', message_info.encode('utf-8'))
+        sleep(0.1)
+        count += 1
+
+# block until all async messages are sent
+producer.flush()
+
+# configure multiple retries
+producer = KafkaProducer(retries=5)
