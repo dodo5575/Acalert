fc5f01e2727167ff5f87f466d64b978a83ad22f2
Chen-Yu Li <dodo5575@gmail.com>
2017-09-25 19:17:07 -0700

    add flask

---
+++ flask/app/static/rethinkDB.js
@@ -0,0 +1,51 @@
+var socket = io('http://' + document.domain + ':' + location.port);
+
+socket.on('connect', function() {
+    console.log("connected");
+});
+
+socket.on('components', function(msg) {
+    var newData = msg.data.new_val;
+    //console.log(newData);
+    processData(newData);
+});
+
+socket.on('error', function(err) {
+    console.log("error returned: ", err);
+});
+
+function processData(allRows) {
+
+  console.log(allRows);
+  console.log(allRows['status']);
+  
+  var label = 'userid_'+allRows['userid'];
+
+  document.getElementById(label).innerHTML = allRows['status'];
+
+  if (allRows['status'] == 'safe') {
+    document.getElementById(label).style.backgroundColor = '#82E0AA';
+  } else {
+    document.getElementById(label).style.backgroundColor = '#E74C3C';
+  }
+  //var x = [], y = [], standard_deviation = [];
+
+  //for (var i=0; i<allRows.length; i++) {
+  //  row = allRows[i];
+  //  x.push( i );
+  //  y.push( row['xacc'] );
+  //}
+  //console.log( 'X',x, 'Y',y, 'SD',standard_deviation );
+  //makePlotly( x, y, standard_deviation );
+};
+
+function makePlotly( x, y, standard_deviation ){
+  var plotDiv = document.getElementById("plot");
+  var traces = [{
+    x: x,
+    y: y
+  }];
+
+  Plotly.newPlot('chart1', traces,
+    {title: 'Plotting CSV data from AJAX call'});
+};
---
+++ flask/app/views.py
@@ -0,0 +1,91 @@
+# jsonify creates a json representation of the response
+from flask import jsonify, request
+from flask import render_template
+from flask_socketio import SocketIO, emit
+import rethinkdb as r
+
+from app import app
+
+# importing Cassandra modules from the driver we just installed
+from cassandra.cluster import Cluster
+
+socketio = SocketIO(app)
+
+# Setting up connections to cassandra
+
+# Change the bolded text to your seed node public dns (no < or > symbols but keep quotations. Be careful to copy quotations as it might copy it as a special character and throw an error. Just delete the quotations and type them in and it should be fine. Also delete this comment line
+cluster = Cluster(['ec2-35-162-98-222.us-west-2.compute.amazonaws.com'])
+
+# Change the bolded text to the keyspace which has the table you want to query. Same as above for < or > and quotations. Also delete this comment line
+session = cluster.connect('playground')
+
+#@app.route('/')
+#@app.route('/index')
+#def index():
+#  user = { 'nickname': 'Miguel' } # fake user
+#  mylist = [1,2,3,4]
+#  return render_template("index.html", title = 'Home', user = user, mylist = mylist)
+#
+#@app.route('/api/<email>/<date>')
+#def get_email(email, date):
+#       stmt = "SELECT * FROM email WHERE id=%s and date=%s"
+#       response = session.execute(stmt, parameters=[email, date])
+#       response_list = []
+#       for val in response:
+#            response_list.append(val)
+#       jsonresponse = [{"first name": x.fname, "last name": x.lname, "id": x.id, "message": x.message, "time": x.time} for x in response_list]
+#       return jsonify(emails=jsonresponse)
+
+def bg_rethink():
+    conn = r.connect(host="localhost", port=28016, db="test")
+    ccCursor = r.table("status").changes().run(conn)
+    for cc in ccCursor:
+        socketio.emit('components', {'data': cc}, json=True)
+        socketio.sleep(0.1)
+
+thread = None
+@socketio.on('connect')
+def connected():
+    print('connected')
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(target=bg_rethink)
+
+@socketio.on('disconnect')
+def disconnected():
+    print('disconnected')    
+    
+@app.route('/')
+def hello():
+    #return render_template("movingTrace.html")
+    return render_template("index.html")
+
+
+@app.route('/api/<id>')
+def get_data(id):
+    stmt = "SELECT userid, time, xacc FROM data WHERE userid=%s limit 25"
+    response = session.execute(stmt, parameters=[int(id)])
+    response_list = []
+    for val in response:
+        response_list.append(val)
+    jsonresponse = [{"userid": x.userid, "time": x.time, "xacc": x.xacc} for x in response_list]
+    return render_template("index.html", data = jsonresponse) 
+
+@app.route('/_add_numbers')
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    stmt = "SELECT userid, time, xacc FROM data WHERE userid=%s limit %s"
+    response = session.execute(stmt, parameters=[a, b])
+    response_list = []
+    for val in response:
+        response_list.append(val)
+    jsonresponse = [{"userid": x.userid, "time": x.time, "xacc": x.xacc} for x in response_list]
+    return jsonify(result=jsonresponse)
+
+
+#@app.route('/movingTrace')
+#def movingTrace():
+#    return render_template("movingTrace.html")
+
