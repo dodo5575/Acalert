2e0174a511870b75e3532d48aef17608031c7da4
Chen-Yu Li <dodo5575@gmail.com>
2017-09-30 14:38:05 -0700

    clean up kafka

--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -1,3 +1,24 @@
+############################################################
+# This python script is a producer for kafka. It creates 
+# random user data and send to kafka. The data is in JSON
+# format. Here is the schema:
+#
+# {"userid": text, 
+#  "time": timestamp, 
+#  "acc": float}
+# The "acc" column is the acceleration of the user.
+
+# To send it to kafka, each record is first converted to 
+# string then to bytes using str.encode('utf-8') method.
+#
+# The parameters
+# config.KAFKA_SERVERS: public DNS and port of the servers
+# config.ANOMALY_PERIOD: How often to create an outlier  
+# config.ANOMALY_VALUE: The value to add to create an outlier
+# were written in a separate "config.py".
+############################################################
+
+
 import random
 import sys
 import datetime
@@ -5,36 +26,42 @@ import numpy as np
 from kafka import KafkaProducer
 from time import sleep
 
+# configuration file
+import config
 
-NUM_SPAWNS = int(sys.argv[1])
-ID = int(sys.argv[2])
 
-nUsers = 200
-users_array = np.array(range(nUsers))
-subUser_array = np.array_split(users_array, NUM_SPAWNS)[ID]
+# number of users in the system
+nUsers = int(sys.argv[1])
 
-producer = KafkaProducer(bootstrap_servers='ec2-34-214-188-4.us-west-2.compute.amazonaws.com:9092,ec2-52-42-208-185.us-west-2.compute.amazonaws.com:9092,ec2-35-163-245-197.us-west-2.compute.amazonaws.com:9092')
+producer = KafkaProducer(bootstrap_servers = config.KAFKA_SERVERS)
 
 count = 0
 while True:
 
-    for userid_field in subUser_array:
+    for userid_field in range(nUsers):
         time= datetime.datetime.now() 
-        time_field = time.strftime("%Y-%m-%d %H:%M:%S")
-    
-        acc_field = np.random.randn()
-        if count % 333 == 0:
-            acc_field += 10  # Add anomaly
 
-        message_info = '{"userid": "%s", "time": "%s", "acc": "%s"}' % (userid_field, time_field, acc_field)
+        # There could be more than 1 record per user per second, so microsecond is added to make each record unique.
+        time_field = time.strftime("%Y-%m-%d %H:%M:%S %f")
     
-        #print(message_info)
+        acc_field = np.random.randn() # generate random acceleration
+
+        if count % config.ANOMALY_PERIOD == 0:
+            acc_field += config.ANOMALY_VALUE  # Add anomaly
+
+        message_info = '{"userid": "%s", "time": "%s", "acc": "%s"}' \
+                       % (userid_field, time_field, acc_field)
+    
         producer.send('data', message_info.encode('utf-8'))
-        #sleep(0.01)
+
+        # In python3, there is no longer a limit to the maximum value of integers
         count += 1
+
 
 # block until all async messages are sent
 producer.flush()
 
 # configure multiple retries
 producer = KafkaProducer(retries=5)
+
+
