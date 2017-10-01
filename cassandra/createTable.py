ded2de1c47d084679a3724e65efa425fa672cd2b
Chen-Yu Li <dodo5575@gmail.com>
2017-10-01 10:51:28 -0700

    clean up cassandra

---
+++ cassandra/createTable.py
@@ -0,0 +1,9 @@
+from cassandra.cluster import Cluster
+import config
+
+cluster = Cluster(config.CASSANDRA_SERVER)
+session = cluster.connect('playground')
+
+session.execute('DROP TABLE IF EXISTS data;')
+session.execute('CREATE TABLE data (userid int, time timestamp, acc float, mean float, std float, status text, PRIMARY KEY (userid, time) ) WITH CLUSTERING ORDER BY (time DESC);')
+
